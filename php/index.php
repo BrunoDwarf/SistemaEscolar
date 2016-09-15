2ccbcd41f7d0782846caa2deca67de834c8a9904
Bruno R Souza <bruno.rodriguesouza1@gmail.com>
2016-09-15 15:25:10 -0300

    Fazendo index

---
+++ php/index.php
@@ -0,0 +1,10 @@
+<?php require_once "templates/header/header.php" ?>
+    <!-- AREA DE CONTEUDO -->
+    <div id="section">
+      <div id="form">
+        <p><h3>Desenvolvido por:</h3> <blockquote>Bruno Rodrigues Souza N:1 <br/> Matheus Resende N:14</blockquote></p>
+      </div>
+    </div>
+
+    <!-- JAVASCRIPT-->
+<?php require_once "templates/footer/footer.php" ?>
