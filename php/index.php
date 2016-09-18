f791a0a4ea9f8225d9cdbc0b9e0b64730e953908
Bruno R Souza <bruno.rodriguesouza1@gmail.com>
2016-09-17 22:23:03 -0300

    Modificação do Index

--- php/index.php
+++ php/index.php
@@ -1,8 +1,118 @@
 <?php require_once "templates/header/header.php" ?>
     <!-- AREA DE CONTEUDO -->
-    <div id="section">
-      <div id="form">
-        <p><h3>Desenvolvido por:</h3> <blockquote>Bruno Rodrigues Souza N:1 <br/> Matheus Resende N:14</blockquote></p>
+    <div class="section">
+      <div class="center-block">
+        <div class="panel panel-info">
+          <div class="panel-heading">Texto de Boas Vindas</div>
+          <div class="panel-body">
+            <h4>Sejam Todos Bem Vindos</h4>
+            <p>Primeiro de tudo, gostaria de agradecer
+               a todos pela preferencia e por estarem
+               ajudando a gente com este trabalho que
+               pode (e vai) crescer muito. Por enquanto
+               só conseguimos disponibilizar esses serviços:
+               <table class="table table-condensed">
+                 <thead>
+                   <tr>
+                     <th>Funções/
+                        Categorias</th>
+                     <th>Cadastrar</th>
+                     <th>Consultar</th>
+                     <th>Editar</th>
+                     <th>Deletar</th>
+                     <th>Gerar PDF</th>
+                   </tr>
+                 </thead>
+                 <tbody>
+                   <tr>
+                     <td>Disciplina</td>
+                     <td class="success">OK</td>
+                     <td class="success">OK</td>
+                     <td class="success">OK</td>
+                     <td class="success">OK</td>
+                     <td>-</td>
+                   </tr>
+                   <tr>
+                     <td>Professor</td>
+                     <td class="success">OK</td>
+                     <td class="success">OK</td>
+                     <td class="success">OK</td>
+                     <td class="success">OK</td>
+                     <td>-</td>
+                   </tr>
+                   <tr>
+                     <td>Questão</td>
+                     <td class="success">OK</td>
+                     <td class="success">OK</td>
+                     <td class="success">OK</td>
+                     <td class="success">OK</td>
+                     <td>-</td>
+                   </tr>
+                   <tr>
+                     <td>Assunto
+                       da Questão</td>
+                     <td class="danger">N</td>
+                     <td class="danger">N</td>
+                     <td class="danger">N</td>
+                     <td class="danger">N</td>
+                     <td>-</td>
+                   </tr>
+                   <tr>
+                     <td>Dificuldade
+                       da Questão</td>
+                     <td class="danger">N</td>
+                     <td class="danger">N</td>
+                     <td class="danger">N</td>
+                     <td class="danger">N</td>
+                     <td>-</td>
+                   </tr>
+                   <tr>
+                     <td>Ano Escolar</td>
+                     <td class="danger">N</td>
+                     <td class="danger">N</td>
+                     <td class="danger">N</td>
+                     <td class="danger">N</td>
+                     <td>-</td>
+                   </tr>
+                   <tr>
+                     <td>Prova</td>
+                     <td class="success">OK</td>
+                     <td class="success">OK</td>
+                     <td class="success">OK</td>
+                     <td class="success">OK</td>
+                     <td class="success">OK</td>
+                   </tr>
+                 </tbody>
+               </table>
+               Espero que essas funcionalidades vocês consigam suprir suas
+               necessidades diarias.</p>
+            <p>Como todos bem sabem, este sistema ainda
+               em desenvolvimento então é comum ele apresentar
+               falhas. Caso, venha ocorrer algum tipo de erro
+               ou simplesmente quiserem nos contatar seja para
+               pedir alguma funcionalidade nova ou só para opinarem
+               sobre o sistema em si, mandem-nos um e-mail.</p>
+          </div>
+          <div class="panel-footer">
+            <address>
+              <strong>Email</strong><br>
+              testedelog6@gmail.com
+            </address>
+
+            <address>
+              <strong>Assunto do E-mail</strong><br>
+              Erro + tipo_do_erro/nome_do_erro/local_do_erro<br>
+              Sugestão<br>
+              Opinião<br>
+            </address>
+
+            <address>
+              <strong>OBS</strong><br>
+              E-mail sem assunto definido não será nem aberto
+              então <strong>NÃO SE ESQUEÇAM DO ASSUNTO</strong>.
+            </address>
+          </div>
+        </div>
       </div>
     </div>
 
